1135428df169018330e98fa1dbf541272ffe9824
Yoyingyang6367 <zhaoyueyang5@gmail.com>
2024-10-16 00:57:05 +1300

    Add files via upload

---
+++ pair work database.sql
@@ -0,0 +1,13 @@
+---------information fors location-----
+INSERT INTO Location(Location_ID,Location_Name,Location_Latitude,Location_Longitude, Location_Description) 
+VALUES ('LC001',); 
+INSERT INTO Location(Location_ID,Location_Name,Location_Latitude,Location_Longitude, Location_Description) 
+VALUES ('LC002',); 
+INSERT INTO Location(Location_ID,Location_Name,Location_Latitude,Location_Longitude, Location_Description) 
+VALUES ('LC003',); 
+INSERT INTO Location(Location_ID,Location_Name,Location_Latitude,Location_Longitude, Location_Description) 
+VALUES ('LC004',); 
+INSERT INTO Location(Location_ID,Location_Name,Location_Latitude,Location_Longitude, Location_Description) 
+VALUES ('LC005',);
+----------------------------- 
+
